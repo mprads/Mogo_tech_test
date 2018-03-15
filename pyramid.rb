60552374a81b56cc0bcf8c92e0db9014b39c64f2
Myles Rademaker <mylesprademaker@gmail.com>
2018-03-15 15:27:59 -0700

    Added error handling and removed double quotes

--- pyramid.rb
+++ pyramid.rb
@@ -1,11 +1,15 @@
 sample = [1,2,3,4,5,6]
 
 def pyramid(array, index = 0)
-  return if index == array.length
-  value = array[index].to_s
-  puts " " * index + "<" + value + ">"
-  pyramid(array, index + 1)
-  puts " " * index + "</" + value + ">"
+  if array.instance_of? Array
+    return if index == array.length
+    value = array[index].to_s
+    puts ' ' * index + '<' + value + '>'
+    pyramid(array, index + 1)
+    puts ' ' * index + '</' + value + '>'
+  else 
+    puts 'Sorry this method only accepts arrays.'
+  end
 end
 
 pyramid(sample)

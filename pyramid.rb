72e6886f4ab919fbb78e15f44446fd8c95d45b72
Myles Rademaker <mylesprademaker@gmail.com>
2018-03-15 10:43:38 -0700

    Removed state and side effects from recursion

--- pyramid.rb
+++ pyramid.rb
@@ -1,34 +1,11 @@
 sample = [1,2,3,4,5,6]
 
-class Pyramid
-  def initialize(array)
-    @array = array
-    @index = 0
-    @direction = :right
-  end
-  
-  def print
-    if @array.instance_of? Array
-      if @index < @array.length && @direction == :right
-        output = "<#{@array[@index]}>"
-        puts output.rjust(output.length + @index, ' ')
-        if @index == @array.length - 1
-          @direction = :left
-        else
-          @index += 1
-        end
-        print
-      elsif @index >= 0 && @direction == :left
-        output = "</#{@array[@index]}>"
-        puts output.rjust(output.length + @index, ' ')
-        @index -= 1
-        print
-      end
-    else
-    puts 'Please make sure you are passing an array to the class'
-    end
-  end
+def pyramid(array, index = 0)
+  return if index == array.length
+  value = array[index].to_s
+  puts " " * index + "<" + value + ">"
+  pyramid(array, index + 1)
+  puts " " * index + "</" + value + ">"
 end
 
-pyramid = Pyramid.new(sample);
-pyramid.print
+pyramid(sample)

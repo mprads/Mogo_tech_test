b118e8ba0d3d2bf5be7016abcf29b51e69085cf5
Myles Rademaker <mylesprademaker@gmail.com>
2018-03-15 15:29:23 -0700

    Removed logic from file and move to models then required models

--- dog_bones/dog_bones_ar_query.rb
+++ dog_bones/dog_bones_ar_query.rb
@@ -5,26 +5,10 @@ require 'active_record'
 require 'mysql2'
 require 'yaml'
 
+require './models/bone'
+require './models/dog.rb'
+
 # Establish our connection to our serverless mysql database.
 ActiveRecord::Base.establish_connection(YAML::load(File.open('config/database.yml')))
 
-class Dog < ActiveRecord::Base
-  has_many :bones
-end
-
-class Bone < ActiveRecord::Base
-  belongs_to :dogs
-end
-
-@dogs = Dog.all
-@dog_bones_data = @dogs.map do |dog|
-  @ratings = dog.bones.pluck(:rating)
-  @count = @ratings.length
-  if @ratings.length == 0
-    @average = 0
-  else
-    @average = @ratings.inject(0.0) { |sum, el| sum + el } / @ratings.length
-  end
-  @data = {'id' => dog.id, 'bones_quantity' => @count, 'average_rating' => @average}
-end
-puts @dog_bones_data
+puts Dog.all_dogs_bones_and_rating

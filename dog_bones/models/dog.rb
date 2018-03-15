7b6db6900ff2ad7e39ccfb32b7435966d2fa9ceb
Myles Rademaker <mylesprademaker@gmail.com>
2018-03-15 15:28:54 -0700

    Moved bone count and rating logic into model

--- dog_bones/models/dog.rb
+++ dog_bones/models/dog.rb
@@ -1,3 +1,21 @@
 class Dog < ActiveRecord::Base
   has_many :bones
+  
+  def self.all_dogs_bones_and_rating
+    dogs = Dog.all
+    dogs.map do |dog|
+      Dog.get_bones_and_rating(dog)
+    end
+  end
+  
+  def self.get_bones_and_rating(dog)
+    ratings = dog.bones.pluck(:rating)
+    count = ratings.length
+    if ratings.length == 0
+      average = 0
+    else
+      average = ratings.inject(0.0) { |sum, el| sum + el } / ratings.length
+    end
+    {'id' => dog.id, 'bones_quantity' => count, 'average_rating' => average}
+  end
 end
